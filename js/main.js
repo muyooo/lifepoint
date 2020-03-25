69fa84d4049f5d3adf0d721ce08e189632a18266
muyooo <muyooo.info@gmail.com>
2020-03-25 21:50:50 +0900

    Added button function that use point.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+{
+  /*
+   * Button function - Use point
+   */
+  const pointBtn = document.querySelector('.point__btn');
+  pointBtn.addEventListener('click', () => {
+    let life = document.querySelectorAll('.point__life:not(.point__life--used)'),
+        lastlife = life[life.length - 1],
+        lastlifeIcon = lastlife.querySelector('.point__icon');
+    lastlife.classList.add('point__life--used');
+    lastlifeIcon.style.display = 'none';
+  });
+}

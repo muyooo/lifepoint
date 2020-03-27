e73bb4fcb146d6404ebd5eb4eb842c19cfd6f283
muyooo <muyooo.info@gmail.com>
2020-03-27 23:17:06 +0900

    Deduplicate for current time returning

--- js/main.js
+++ js/main.js
@@ -6,14 +6,18 @@
   /* ------------------------------ */
   // -- Return Now Date
   const getNowDate = () => {
-    const nd = new Date(),
+    const get2Digits = (target) => {
+            const result = ('0' + target).slice(-2);
+            return result;
+          },
+          nd = new Date(),
           ndYear = nd.getFullYear(),
           ndMonth = nd.getMonth() + 1,
-          ndMonth2dig = ('0' + ndMonth).slice(-2),
-          ndDay2dig = ('0' + nd.getDate()).slice(-2),
-          ndHour2dig = ('0' + nd.getHours()).slice(-2),
-          ndMinutes2dig = ('0' + nd.getMinutes()).slice(-2),
-          ndSeconds2dig = ('0' + nd.getSeconds()).slice(-2),
+          ndMonth2dig = get2Digits(ndMonth),
+          ndDay2dig = get2Digits(nd.getDate()),
+          ndHour2dig = get2Digits(nd.getHours()),
+          ndMinutes2dig = get2Digits(nd.getMinutes()),
+          ndSeconds2dig = get2Digits(nd.getSeconds()),
           returnDate = {
             pointTitle: ndYear + '年' + ndMonth + '月',
             nowDate: ndYear + '-' + ndMonth2dig + '-' + ndDay2dig + ' ' + ndHour2dig + ':' + ndMinutes2dig + ':' + ndSeconds2dig

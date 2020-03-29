986c8b5967796151789dc2a00483b557e258cf44
muyooo <muyooo.info@gmail.com>
2020-03-30 00:23:34 +0900

    Fixed bug that output error when use point with empty rest point.

--- js/main.js
+++ js/main.js
@@ -72,8 +72,12 @@
   // -- Button function - Use point
   const pointBtn = document.querySelector('.point__btn');
   pointBtn.addEventListener('click', () => {
-    let life = document.querySelectorAll('.point__life:not(.point__life--used)'),
-        lastlife = life[life.length - 1],
+    let life = document.querySelectorAll('.point__life:not(.point__life--used)');
+    if (life.length == 0) {
+      alert('残りポイントがありません。');
+      return;
+    }
+    let lastlife = life[life.length - 1],
         lastlifeIcon = lastlife.querySelector('.point__icon');
     lastlife.classList.add('point__life--used');
     lastlifeIcon.style.display = 'none';
